be678574f810544066f426a5eec02fcdd2b080ff
RomyToklio <contact@evaveo.com>
2018-10-11 15:39:25 +0200

    up

--- external-libs/script/boost-build.sh
+++ external-libs/script/boost-build.sh
@@ -6,8 +6,8 @@ source script/env.sh
 
 TARGET_DIR=$EXTERNAL_LIBS_ROOT/boost
 
-version=1_58_0
-dot_version=1.58.0
+version=1_62_0
+dot_version=1.62.0
 
 cd $EXTERNAL_LIBS_BUILD_ROOT/boost_${version}
 
